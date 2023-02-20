20d7a2f30bc9c251b2789d01b833d1fe6a13a7d8
Amritrajdubey <amrit224raj@gmail.com>
2023-02-21 00:22:20 +0530

    updated data validation file

--- sensor/entity/artifact_entity.py
+++ sensor/entity/artifact_entity.py
@@ -2,8 +2,13 @@ from dataclasses import dataclass
 
 @dataclass
 class DataIngestionArtifact:
-    pass
+    feature_store_file_path:str
+    train_file_path:str
+    test_file_path:str 
 
+@dataclass
+class DataValidationArtifact:
+    report_file_path : str
 
 class DataValidationArtifact :...
 class DataTransformationArtifact :...
